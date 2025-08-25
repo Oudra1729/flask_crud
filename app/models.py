470118361a7d5e445e8be4209740682cebe0dcb7
oudra1729 <brahimbra12356@gmail.com>
2025-08-25 23:51:06 +0100

    test

--- app/models.py
+++ app/models.py
@@ -3,3 +3,12 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    
+    
+    
