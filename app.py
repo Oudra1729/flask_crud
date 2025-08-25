5f8cd4814d2304d3337eac082fffde8569d5663a
oudra1729 <brahimbra12356@gmail.com>
2025-08-25 20:25:03 +0100

    v0: upate

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# إعدادات PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/flaskdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# مثال: جدول المستخدمين
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     
-    
-    
-
 with app.app_context():
     db.create_all()
 
 
 @app.route('/')
 def index():
-    return "الربط بين Flask و PostgreSQL ناجح"
-
+    return "Connection between Flask and PostgreSQL successful"
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
@@ -40,30 +35,30 @@ def add_user():
     new_user = User(name=data['name'])
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"message": "تمت إضافة المستخدم بنجاح!"})
+    return jsonify({"message": "User added successfully!"})
 
 @app.route('/update_user/<int:id>', methods=['PUT'])
 def update_user(id):
     user = User.query.get(id)
     if not user:
-        return jsonify({"error": "المستخدم غير موجود"}), 404
+        return jsonify({"error": "User not found"}), 404
     
     data = request.get_json()
     user.name = data['name']
     db.session.commit()
-    
-    return jsonify({"message": "تم تعديل المستخدم بنجاح"})
+
+    return jsonify({"message": "User updated successfully!"})
 
 @app.route('/delete_user/<int:id>', methods=['DELETE'])
 def delete_user(id):
     user = User.query.get(id)
     if not user:
-        return jsonify({"error": "المستخدم غير موجود"}), 404
-    
+        return jsonify({"error": "User not found"}), 404
+
     db.session.delete(user)
     db.session.commit()
-    
-    return jsonify({"message": "تم حذف المستخدم بنجاح"})
+
+    return jsonify({"message": "User deleted successfully!"})
 
 
 if __name__ == '__main__':
